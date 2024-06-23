c896d514a13c164684da0bf7c92084c77e3ca709
Shoyeb Sayyad <113976085+shoyeb03@users.noreply.github.com>
2024-06-23 23:47:28 +0530

    Create 1_HelloWorld.java

---
+++ 1_HelloWorld.java
@@ -0,0 +1,13 @@
+package shoyeb;
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Hello");
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println(name);
+    }
+}
